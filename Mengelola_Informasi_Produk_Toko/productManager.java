865a49f6525160774dbfd377217e0041faf2a3a2
derukhdj22@gmail.com <derukhdj22@gmail.com>
2024-10-04 22:06:34 +0700

    output dari class product

---
+++ Mengelola_Informasi_Produk_Toko/productManager.java
@@ -0,0 +1,7 @@
+package Mengelola_Informasi_Produk_Toko;
+
+public class productManager {
+    public void tampilkanInformasiProduk(product product) {
+        product.printProductionInfo();
+    }
+}
